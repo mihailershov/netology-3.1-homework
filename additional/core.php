4b446da02dbbabf007a4fe72b211062231a3e164
Mihail Ershov <mihershov9@gmail.com>
2017-05-14 14:13:34 +0300

    was separated interfaces

--- additional/core.php
+++ additional/core.php
@@ -2,15 +2,15 @@
 
 session_start();
 // Подключаем суперкласс и интерфейс суперкласса
-require_once 'classes/TheNewsTemplateInterface.php';
+require_once 'classes/interfaces/TheNewsTemplateInterface.php';
 require_once 'classes/TheNewsTemplate.php';
 
 // Подключаем наследников - новость, комментарий, а также интерфейс новости.
-require_once 'classes/NewsInterface.php';
+require_once 'classes/interfaces/NewsInterface.php';
 require_once 'classes/News.php';
 require_once 'classes/Comment.php';
 
 // Подключаем функции для работы и интерфейс новостей
-require_once 'classes/FunctionsInterface.php';
+require_once 'classes/interfaces/FunctionsInterface.php';
 require_once 'classes/Functions.php';
 $functions = new Functions;
